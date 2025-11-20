7c4749fad238450c1ab6c2876bc00faff10c640d
Inika Bhargava <inikamehro@gmail.com>
2025-11-19 17:29:17 -0800

    Adding changes

--- animals_task7.pl
+++ animals_task7.pl
@@ -1,40 +1,60 @@
-% --- Facts: Animal categories ---
-animal(dog).
-animal(cat).
-animal(elephant).
-animal(eagle).
-animal(shark).
-animal(dolphin).
-animal(crocodile).
-animal(penguin).
-animal(salmon).
-animal(turtle).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Animal Reasoning: Warm-Blooded + Swimming Ability
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% --- Facts: Land and water animals ---
+%%%%%%%%%%%%%%% RULES %%%%%%%%%%%%%%%
+
+%% An animal is warm-blooded if it is a mammal.
+warm_blooded(Animal) :-
+    mammal(Animal).
+
+%% An animal is warm-blooded if it is a bird.
+warm_blooded(Animal) :-
+    bird(Animal).
+
+%% An animal can swim if it is a water animal.
+can_swim(Animal) :-
+    water_animal(Animal).
+
+%% Penguins can swim even though they are land birds.
+can_swim(penguin).
+
+
+%%%%%%%%%%%%%%% FACTS %%%%%%%%%%%%%%%
+
+%% Animal categories
+mammal(dog).
+mammal(cat).
+mammal(elephant).
+mammal(dolphin).
+
+bird(eagle).
+bird(penguin).
+
+fish(shark).
+fish(salmon).
+
+reptile(crocodile).
+reptile(turtle).
+
+%% Habitat facts
 land_animal(dog).
 land_animal(cat).
 land_animal(elephant).
-land_animal(penguin).  % penguins live on land but swim
+land_animal(penguin).
+
 water_animal(shark).
 water_animal(dolphin).
 water_animal(salmon).
 water_animal(crocodile).
 water_animal(turtle).
 
-% --- Facts: Traits ---
-mammal(dog).
-mammal(cat).
-mammal(elephant).
-mammal(dolphin).
-bird(eagle).
-bird(penguin).
-fish(shark).
-fish(salmon).
-reptile(crocodile).
-reptile(turtle).
+
+%%%%%%%%%%%%%%% TRAITS (optional) %%%%%%%%%%%%%%%
 
 has_fur(dog).
 has_fur(cat).
+
 lays_eggs(eagle).
 lays_eggs(penguin).
 lays_eggs(shark).
@@ -42,10 +62,18 @@ lays_eggs(salmon).
 lays_eggs(crocodile).
 lays_eggs(turtle).
 
-% --- Rule: warm-blooded animals are mammals or birds ---
-warm_blooded(X) :- mammal(X).
-warm_blooded(X) :- bird(X).
 
-% --- Rule: can_swim if animal is water_animal or penguin ---
-can_swim(X) :- water_animal(X).
-can_swim(penguin).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Example Queries:
+%%
+%% ?- warm_blooded(dog).            % true
+%% ?- warm_blooded(shark).          % false
+%%
+%% ?- can_swim(dolphin).            % true
+%% ?- can_swim(penguin).            % true (special rule)
+%% ?- can_swim(elephant).           % false
+%%
+%% ?- mammal(X).                    % list all mammals
+%% ?- warm_blooded(X).              % list all warm-blooded animals
+%% ?- can_swim(X).                  % list all animals that can swim
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
